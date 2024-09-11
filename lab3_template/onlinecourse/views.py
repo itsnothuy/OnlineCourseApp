9c398b8834d7979746d1d62eb05d009627eb3fa7
itsnothuy <huytrngqu@gmail.com>
2024-09-11 16:00:10 -0400

    Final commit

---
+++ lab3_template/onlinecourse/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render, redirect
+from .models import Course, Lesson, Enrollment
+from django.urls import reverse
+from django.views import generic
+from django.http import Http404
+
+# Create your views here.
+def enroll(request, course_id):
+    # If request method is POST
+    if request.method == 'POST':
+        # First try to read the course object
+        # If could be found, raise a 404 exception
+        course = get_object_or_404(Course, pk=course_id)
+        # Increase the enrollment by 1
+        course.total_enrollment += 1
+        course.save()
+        # Return a HTTP response redirecting user to course list view
+        return HttpResponseRedirect(reverse(viewname='onlinecourse:popular_course_list'))
+
+def course_details(request, course_id):
+    context = {}
+    if request.method == 'GET':
+        try:
+            course = Course.objects.get(pk=course_id)
+            context['course'] = course
+            # Use render() method to generate HTML page by combining
+            # template and context
+            return render(request, 'onlinecourse/course_detail.html', context)
+        except Course.DoesNotExist:
+            # If course does not exist, throw a Http404 error
+            raise Http404("No course matches the given id.")
